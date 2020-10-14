2254dd77400478f11d27f89fbb9424004746c0ee
Мурад <52107353+Poloten4ik@users.noreply.github.com>
2020-10-14 17:12:23 +0300

    test hm

---
+++ Assets/Scripts/ExitScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitScene : MonoBehaviour
+{
+    public Text result;
+
+    private LevelSO right;
+    void Start()
+    {
+        right = FindObjectOfType<LevelSO>();
+        float number = right.health;
+        float number2 = right.questions.Length;
+        float number3 = number / number2 * 100;
+        result.text = number3.ToString() + "%";
+        Destroy(right.gameObject);
+    }
+}
--- Assets/Scripts/LevelSO.cs
+++ Assets/Scripts/LevelSO.cs
@@ -1,15 +1,71 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-[CreateAssetMenu(menuName = "lvl", fileName = " new level")]
-public class LevelSO : ScriptableObject
+
+public class LevelSO : MonoBehaviour
 {
-    [TextArea(10,30)]
+
+    public QuestionList[] questions;
+    public Text qText;
+    public Text[] variantsText;
+    public Button[] variantBttns = new Button[4];
+    public int health = 0;
+
+    List<object> qList;
+    QuestionList crntQ;
+    int randQ;
+
+    public void Awake()
+    {
+        qList = new List<object>(questions);
+        QuestionGenerate();
+    }
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    public void QuestionGenerate()
+    {
+        if (qList.Count > 0)
+        {
+            randQ = Random.Range(0, qList.Count);
+            crntQ = qList[randQ] as QuestionList;
+            qText.text = crntQ.question;
+            List<string> variants = new List<string>(crntQ.variants);
+            for (int i = 0; i < crntQ.variants.Length; i++)
+            {
+                int rand = Random.Range(0, variants.Count);
+                variantsText[i].text = variants[rand];
+                variants.RemoveAt(rand);
+            }
+        }
+        else
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+    public void VariantsBttns(int index)
+    {
+        if (variantsText[index].text.ToString() == crntQ.variants[0])
+        {
+            health++;
+        }
+        else
+        {
+
+        }
+        qList.RemoveAt(randQ);
+        QuestionGenerate();
+    }
+}
+
+[System.Serializable]
+public class QuestionList
+{
+    [TextArea(10, 30)]
     public string question;
-    public string[] variant;
-    public Sprite Bg;
-    public LevelSO nextLevel;
-    public int rightVariant;
+    public string[] variants = new string[4];
 }
